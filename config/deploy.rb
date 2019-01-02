cfd6d786be6cc0e25af16f0db3f511df47019e6b
tofujii0629 <46305877+tofujii0629@users.noreply.github.com>
2019-01-02 10:31:01 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,9 @@ lock "~> 3.10.2"
 #
 # あなたのGitHubユーザー名に変更してください
 #
-set :repo_url, "https://github.com/xxxx/20180430-schoo.git"
+set :repo_url, "https://github.com/tofujii0629/20180430-schoo.git"
 
 #
 # あなたのWebサーバーの環境に合わせてデプロイ先ディレクトリパスを設定してください
 #
-set :deploy_to, "/var/www/schoo"
+set :deploy_to, "/var/github/schoo"
